9e6fc7e83281d9f0dbaa6049644a8cb801841e78
song jun <sj97833721@gmail.com>
2022-05-13 19:13:47 +0900

    second commit (nomadcoders #2.5 Arrays)

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+//const => 값 변경 불가능
+//let   => 값 변경 가능
+
 alert("hi");
+
+const a=5;              //integer
+const b=2;              //integer
+let myName="js";        //string
+const amIFat=true;      //boolean
+let somethig;           //undefine
+const tmp=null;         //null
+const dayOfWeek=["mon","tue","wed","thu","fri","sat"];        //array
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+
+console.log("hello "+myName);
+myName="jave script";
+console.log("hello "+myName);
+
+console.log(amIFat);
+console.log(somethig);
+console.log(tmp);
+console.log(dayOfWeek);
+console.log(dayOfWeek[0]);
+
+dayOfWeek.push("sun");
+console.log(dayOfWeek);
