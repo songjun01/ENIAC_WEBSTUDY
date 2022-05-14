d2eab572badc0d366ea89680531bb3b5d361ab7f
song jun <sj97833721@gmail.com>
2022-05-14 23:13:29 +0900

    nomadcoder (#3.1 HTML in Javascript)

---
+++ chapter3/chapter3.js
@@ -0,0 +1,7 @@
+const title=document.getElementById("title");       //html의 element 중에서 id가 "title"인 element를 찾아 title에 저장
+
+//console.dir(title);
+title.innerText="Got you!";                         //title의 text 변경
+
+console.log(title.id);
+console.log(title.className);
