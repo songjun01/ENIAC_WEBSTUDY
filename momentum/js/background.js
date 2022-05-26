9afdc45061c8630a3a2c14d14bea207a9c9a55d6
song jun <sj97833721@gmail.com>
2022-05-26 10:22:11 +0900

    nomadcoder chapter6 finish

---
+++ momentum/js/background.js
@@ -0,0 +1,11 @@
+const images=["0.JPEG","1.JPEG","2.JPEG","3.JPEG"];
+
+const chosenImage=images[Math.floor(Math.random()*images.length)];
+
+const bgImage=document.createElement("img");
+
+bgImage.src=`C:/대학교/ENIAC/Web/nomadcoders chrome app/momentum/img/${chosenImage}`;
+
+console.log(bgImage);
+
+document.body.appendChild(bgImage);
